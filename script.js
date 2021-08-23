ebc8e649e9dbefc0d237ee218dd97b5016bbe96f
ziomekK <mariopesca02@gmail.com>
2021-08-23 15:21:57 +0200

    playRound function first version added + capitalize function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function capitalize(str) {
+    const lower = str.toLowerCase();
+    return str.charAt(0).toUpperCase() + lower.slice(1);
+}
+  
+
 function computerPlay() {
     let num = Math.floor(Math.random() * 3);
     switch (num) {
@@ -18,3 +24,30 @@ function computerPlay() {
             break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    
+    let winner;
+
+    if (playerSelection.toUpperCase()  === computerSelection.toUpperCase()) {
+        winner = "tie";
+        console.log(`You tied! (${capitalize(playerSelection)} vs ${capitalize(computerSelection)})`);
+        return winner;
+
+    } else if ((playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS") ||
+               (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") ||
+               (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER")) {
+    
+        winner = "player";
+        console.log(`You won! (${capitalize(playerSelection)} vs ${capitalize(computerSelection)})`);
+        return winner;
+
+    } else if ((computerSelection.toUpperCase() === "ROCK" && playerSelection.toUpperCase() === "SCISSORS") ||
+               (computerSelection.toUpperCase() === "PAPER" && playerSelection.toUpperCase() === "ROCK") ||
+               (computerSelection.toUpperCase() === "SCISSORS" && playerSelection.toUpperCase() === "PAPER")) {
+
+        winner = "computer";
+        console.log(`Computer won! (${capitalize(playerSelection)} vs ${capitalize(computerSelection)})`);
+        return winner;
+    }    
+  }
